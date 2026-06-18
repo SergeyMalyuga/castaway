01f54f1ac629fdf6d5c76427daef42509efb3b6c
Sergey Malyuga <sergey@malyuga.ru>
2026-06-18 11:44:20 +0300

    feat(home-page): добавить header

--- src/pages/MainPage.ts
+++ src/pages/MainPage.ts
@@ -1,17 +1,34 @@
-import type { MainProps } from "../core/models/MainProps.ts";
-import type { Header } from "../components/header/Header.ts";
+import type {MainProps} from "../core/models/MainProps.ts";
+import type {Header} from "../components/header/Header.ts";
+import type {DataHook} from "../types/DataHook.type.ts";
+import {Hooks} from "../core/constants/consts.ts";
 
 export class MainPage {
-  private container: HTMLElement;
-  private header: Header;
+    private container: HTMLElement;
+    private header: Header;
 
-  public constructor(container: HTMLElement, props: MainProps) {
-    this.container = container;
-    this.header = props.header;
-  }
-  public render() {
-    this.container.innerHTML = `
-        ${this.header.render()}
+    public constructor(container: HTMLElement, props: MainProps) {
+        this.container = container;
+        this.header = props.header;
+    }
+
+    public render() {
+        this.container.innerHTML = `
+                <div data-hook="header"></div>
+        <main>
+
+</main>
         `;
-  }
+        this.mount(this.header.render(), Hooks.HEADER)
+        this.header.init();
+    }
+
+    private mount(element: HTMLElement, data: DataHook) {
+        const container = this.container.querySelector(data) as HTMLElement;
+        container.appendChild(element);
+    }
+
+    public destroy() {
+        this.header.destroy();
+    }
 }
