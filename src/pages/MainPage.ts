57ae9e86aa493fcdfe1b8f2bad9c59d21cfdd066
Sergey Malyuga <sergey@malyuga.ru>
2026-06-18 15:22:36 +0300

    feat(home-page): добавить секцию hero

--- src/pages/MainPage.ts
+++ src/pages/MainPage.ts
@@ -2,21 +2,24 @@ import type {MainProps} from "../core/models/MainProps.ts";
 import type {Header} from "../components/header/Header.ts";
 import type {DataHook} from "../types/DataHook.type.ts";
 import {Hooks} from "../core/constants/consts.ts";
+import type {Hero} from "../components/hero/Hero.ts";
 
 export class MainPage {
     private container: HTMLElement;
     private header: Header;
+    private hero: Hero;
 
     public constructor(container: HTMLElement, props: MainProps) {
         this.container = container;
         this.header = props.header;
+        this.hero = props.hero;
     }
 
     public render() {
         this.container.innerHTML = `
                 <div data-hook="header"></div>
         <main>
-
+${this.hero.render()}
 </main>
         `;
         this.mount(this.header.render(), Hooks.HEADER)
