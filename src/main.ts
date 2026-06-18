c287a120f57c333193335ce2b1554ac352d15d8a
Sergey Malyuga <sergey@malyuga.ru>
2026-06-18 15:21:43 +0300

    feat(main): создать экземпляр класса Hero

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import "./style.scss";
 import { MainPage } from "./pages/MainPage.ts";
 import { Header } from "./components/header/Header.ts";
+import {Hero} from "./components/hero/Hero.ts";
 
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.getElementById("app") as HTMLElement;
   const header = new Header();
+  const hero = new Hero();
+
   if (container) {
-    const mainPage = new MainPage(container, { header });
+    const mainPage = new MainPage(container, { header, hero });
     mainPage.render();
   }
 });
