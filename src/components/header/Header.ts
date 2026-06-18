852e3537ada4cc46078e38d3ccc487836ebbea7c
Sergey Malyuga <sergey@malyuga.ru>
2026-06-18 11:42:53 +0300

    feat(header): адаптировать меню навигации под мобильные устройства

--- src/components/header/Header.ts
+++ src/components/header/Header.ts
@@ -1,17 +1,22 @@
 import styles from "./Header.module.scss";
 
 export class Header {
-  public render() {
-    return `
-        <footer class="${styles.footer}" id="footer">
-        <div class="${styles.content} container">
+    private burger: HTMLButtonElement | null = null;
+    private navMenu: HTMLElement | null = null;
+
+    public render() {
+        const header = document.createElement("header");
+        header.className = styles.header;
+        header.setAttribute("id", "header");
+        header.innerHTML = `
+            <div class="${styles.content} container">
         <a href="/"><img class="${styles.image}" src="/images/vector/logo-mini.svg" 
         width="160" 
         height="33" 
         alt="Логотип студии" 
         loading="eager"/></a>
         
-        <nav class="${styles.nav}" aria-label="Основная навигация">
+        <nav class="${styles.nav}" id="nav-menu" aria-label="Основная навигация">
         <ul class="${styles.navLinks}">
 <li><a class="${styles.link}" href="#">Home</a></li>
 <li><a class="${styles.link}" href="#">Episodes</a></li>
@@ -20,10 +25,41 @@ export class Header {
 </ul>
 </nav>
 
-<button class="${styles.burgerButton}" type="button">
+<button class="${styles.burgerButton}" 
+type="button"
+aria-haspopup="menu"
+aria-expanded="false"
+aria-label="Открыть меню"
+aria-controls="nav-menu"
+>
 <span class="${styles.middleLine}"></span>
 </button>
 </div>
-</footer>`;
-  }
+    `
+        this.burger = header.querySelector(`.${styles.burgerButton}`);
+        this.navMenu = header.querySelector(`.${styles.nav}`);
+
+        return header;
+    }
+
+    public init() {
+        if (this.burger) {
+            this.burger.addEventListener("click", this.toggleNavMenu);
+        }
+    }
+
+    private toggleNavMenu = () => {
+        if (this.navMenu && this.burger) {
+            const isOpen = this.navMenu.classList.toggle(`${styles.navOpen}`);
+            this.burger.classList.toggle(`${styles.burgerButtonOpen}`);
+            this.burger.setAttribute("aria-expanded", isOpen ? 'true' : 'false');
+            this.burger.setAttribute("aria-label", isOpen ? 'Закрыть меню' : 'Открыть меню');
+        }
+    }
+
+    public destroy() {
+        if (this.burger) {
+            this.burger.removeEventListener("click", this.toggleNavMenu);
+        }
+    }
 }
