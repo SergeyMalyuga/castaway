b41d38cbe2249e221d954018d3637582176531ab
Sergey Malyuga <sergey@malyuga.ru>
2026-06-19 13:10:31 +0300

    feat(home-page): добавить секцию episodes

--- src/pages/MainPage.ts
+++ src/pages/MainPage.ts
@@ -3,16 +3,19 @@ import type {Header} from "../components/header/Header.ts";
 import type {DataHook} from "../types/DataHook.type.ts";
 import {Hooks} from "../core/constants/consts.ts";
 import type {Hero} from "../components/hero/Hero.ts";
+import type {Episodes} from "../components/episodes/Episodes.ts";
 
 export class MainPage {
     private container: HTMLElement;
     private header: Header;
     private hero: Hero;
+    private episodes: Episodes;
 
     public constructor(container: HTMLElement, props: MainProps) {
         this.container = container;
         this.header = props.header;
         this.hero = props.hero;
+        this.episodes = props.episodes;
     }
 
     public render() {
@@ -20,10 +23,12 @@ export class MainPage {
                 <div data-hook="header"></div>
         <main>
 ${this.hero.render()}
+                <div data-hook="episodes"></div>
 </main>
         `;
-        this.mount(this.header.render(), Hooks.HEADER)
+        this.mount(this.header.render(), Hooks.HEADER);
         this.header.init();
+        this.mount(this.episodes.render(), Hooks.EPISODES);
     }
 
     private mount(element: HTMLElement, data: DataHook) {
