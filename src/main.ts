0d2b314f325c158f4c2052623d18aed3711f65ce
Sergey Malyuga <sergey@malyuga.ru>
2026-06-19 13:10:07 +0300

    feat(main): создать экземпляр класса Episodes

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,16 @@ import "./style.scss";
 import { MainPage } from "./pages/MainPage.ts";
 import { Header } from "./components/header/Header.ts";
 import {Hero} from "./components/hero/Hero.ts";
+import {Episodes} from "./components/episodes/Episodes.ts";
 
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.getElementById("app") as HTMLElement;
   const header = new Header();
   const hero = new Hero();
+  const episodes = new Episodes();
 
   if (container) {
-    const mainPage = new MainPage(container, { header, hero });
+    const mainPage = new MainPage(container, { header, hero, episodes });
     mainPage.render();
   }
 });
