1bd63db92b9d96cdeb46f07d4e4253c935530568
Sergey Malyuga <sergey@malyuga.ru>
2026-06-19 15:46:37 +0300

    feat(models): добавить свойство mentor в модель главной страницы

--- src/core/models/MainProps.ts
+++ src/core/models/MainProps.ts
@@ -1,9 +1,11 @@
 import type {Header} from "../../components/header/Header.ts";
 import type {Hero} from "../../components/hero/Hero.ts";
 import type {Episodes} from "../../components/episodes/Episodes.ts";
+import type {Mentor} from "../../components/mentor/Mentor.ts";
 
 export interface MainProps {
     header: Header;
     hero: Hero;
     episodes: Episodes;
+    mentor: Mentor;
 }
