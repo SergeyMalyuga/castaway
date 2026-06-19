2d83fccfe4950cd8d133e3cc0c2ab7184a9f5efb
Sergey Malyuga <sergey@malyuga.ru>
2026-06-19 15:47:11 +0300

    feat(main): создать экземпляр класса Mentor

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,17 @@ import { MainPage } from "./pages/MainPage.ts";
 import { Header } from "./components/header/Header.ts";
 import {Hero} from "./components/hero/Hero.ts";
 import {Episodes} from "./components/episodes/Episodes.ts";
+import {Mentor} from "./components/mentor/Mentor.ts";
 
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.getElementById("app") as HTMLElement;
   const header = new Header();
   const hero = new Hero();
   const episodes = new Episodes();
+  const mentor = new Mentor();
 
   if (container) {
-    const mainPage = new MainPage(container, { header, hero, episodes });
+    const mainPage = new MainPage(container, { header, hero, episodes, mentor });
     mainPage.render();
   }
 });
