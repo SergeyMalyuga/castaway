6d08f1ad6b60704a691bfaf4e4059f8da0f948a2
Sergey Malyuga <sergey@malyuga.ru>
2026-06-19 13:05:41 +0300

    feat(models): добавить свойство episodes в модель главной страницы

--- src/core/models/MainProps.ts
+++ src/core/models/MainProps.ts
@@ -1,7 +1,9 @@
 import type {Header} from "../../components/header/Header.ts";
 import type {Hero} from "../../components/hero/Hero.ts";
+import type {Episodes} from "../../components/episodes/Episodes.ts";
 
 export interface MainProps {
     header: Header;
     hero: Hero;
+    episodes: Episodes;
 }
