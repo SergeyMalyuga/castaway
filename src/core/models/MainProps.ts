ba9047e562e17022bcd69ae05e3a4f9111b3a84e
Sergey Malyuga <sergey@malyuga.ru>
2026-06-22 11:28:38 +0300

    feat(models): добавить свойство subscribe в модель главной страницы

--- src/core/models/MainProps.ts
+++ src/core/models/MainProps.ts
@@ -2,10 +2,12 @@ import type {Header} from "../../components/header/Header.ts";
 import type {Hero} from "../../components/hero/Hero.ts";
 import type {Episodes} from "../../components/episodes/Episodes.ts";
 import type {Mentor} from "../../components/mentor/Mentor.ts";
+import type {Subscribe} from "../../components/subscribe/Subscribe.ts";
 
 export interface MainProps {
     header: Header;
     hero: Hero;
     episodes: Episodes;
     mentor: Mentor;
+    subscribe: Subscribe;
 }
